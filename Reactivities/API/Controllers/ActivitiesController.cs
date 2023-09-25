23961ef8b1269af3e3bfee56d05572a777a62c31
Valeh <amirbekovaleh@gmail.com>
2023-09-25 23:34:27 +0400

    Skeleton 1

---
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ActivitiesController : ControllerBase
+{
+    private readonly DataContext _db;
+
+    public ActivitiesController(DataContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await _db.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        return await _db.Activities.FindAsync(id);
+    }
+}
